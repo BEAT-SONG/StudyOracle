73b5909980700110f1e12f2d260508b6ab7ad524
BEAT-SONG <yjyjthddms@naver.com>
2022-01-14 09:39:14 +0900

    3일차 SQL 마무리 및 TEST

--- DAY3/박송은.sql
+++ DAY3/박송은.sql
@@ -9,8 +9,8 @@
  ORDER BY names DESC;
  
  -- 2. 책 테이블에서 책제목, 저자, 출판일, 금액 순으로 출력하고 가격이 비싼 순으로 나오게 하십시오. 컬럼이름도 확인하세요!!
- SELECT *  FROM bookstbl;
- SELECT names 책제목, author 저자명, to_char(releasedate, 'yyyy-mm-dd') 출판일, price 가격 
+ -- SELECT *  FROM bookstbl;
+ SELECT names 책제목, author 저자, to_char(releasedate, 'yyyy-mm-dd') 출판일, price 가격 
  FROM bookstbl
  ORDER BY idx ASC; 
  
@@ -20,7 +20,8 @@
               , b. author 저자
               , TO_CHAR(b.releasedate, 'yyyy-mm-dd') 출판일
               , b.isbn 책코드번호
-              , b. price 가격 
+           --   , CONCAT(TO_CHAR(b. price, '999,999,999'), '원') 가격 -- 글을 추가할 때 사용하는 함수, 숫자 자리수 나타내는 방법
+              , TO_CHAR(b. price, '999,999,999') || '원' 가격 -- 이것도 글을 추가할 때 사용하는 함수 ||
  FROM divtbl d, bookstbl b
 WHERE d.division = b.division
 ORDER BY idx DESC;
@@ -29,6 +30,7 @@ ORDER BY idx DESC;
 --SELECT * FROM membertbl;
 --SELECT SEQ_MEMBERTBL.CURRVAL  FROM dual;
 -- 한번 팅겨서 이미 27이 들어갔다가 다시 눌러서 28로 된거 같습니다.
+-- NULL 로 들어가 있는 데이터 항목은 아예 작성하지 않아도 된다.
 INSERT INTO membertbl
          (idx, names, levels, addr, mobile, email, userid, password, lastlogindt, loginipaddr)
 VALUES
@@ -36,6 +38,7 @@ VALUES
  
  
 -- 5. 아래와 같이 책의 구분별로 합계와 모든 책의 합계가 같이 나오도록 구현하세요. 롤...어... 
+-- 제일 어려웠던 문제.. (나한테)
 
 --SELECT d.names 장르, b.price 장르별합계금액
 --FROM divtbl d, bookstbl b;
@@ -54,7 +57,7 @@ VALUES
 --WHERE d.division = b.division
 --GROUP BY ROLLUP (d.names);
 
-SELECT NVL(d.names, '--합계--') 장르, SUM(b.price) 장르별합계금액
+SELECT NVL(d.names, '--합계--') 장르, TO_CHAR(SUM(b.price), '999,999,999') 장르별합계금액 -- NULL을 바꾸고 싶을 때는 반드시 NVL 함수를 사용하면 된다. 
 FROM divtbl d, bookstbl b
 WHERE d.division = b.division
 GROUP BY ROLLUP (d.names)
